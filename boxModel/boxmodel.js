ebee9a383339952bab4be99489612a9af0cee204
Rizvi <rizvisharis@gmail.com>
2021-06-03 22:11:24 +0530

    boxmodel css file commited

---
+++ boxModel/boxmodel.js
@@ -0,0 +1,8 @@
+const awesome = document.querySelector('[data-element ="awesome"]');
+const intrinsicSwitch = document.querySelector("#intrinsic-switch");
+
+intrinsicSwitch.addEventListener("change",() =>{
+    awesome.setAttribute(
+        "data-sizing",intrinsicSwitch.checked ? "intrinsic" : "extrinsic"
+    );
+});
